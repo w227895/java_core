648d063b76adfd0ffa02a4c4ec4421f93c0a6e8c
kebo <249183617@qq.com>
2020-08-15 10:45:42 +0800

    提交exception的代码

---
+++ java_exception/src/com/kebo/TestFinally.java
@@ -0,0 +1,74 @@
+package com.kebo;
+
+/**
+ * @description: 测试java中的finally
+ * @author: kb
+ * @create: 2020-08-15 10:21
+ **/
+public class TestFinally {
+    public TestFinally() {
+    }
+
+    boolean testEx() throws Exception {
+        boolean ret = true;
+        try {
+            ret = testEx1();
+        } catch (Exception e) {
+            System.out.println("testEx, catch exception");
+            ret = false;
+            throw e;
+        } finally {
+            System.out.println("testEx, finally; return value=" + ret);
+            return ret;
+        }
+    }
+
+    boolean testEx1() throws Exception {
+        boolean ret = true;
+        try {
+            ret = testEx2();
+            if (!ret) {
+                return false;
+            }
+            System.out.println("testEx1, at the end of try");
+            return ret;
+        } catch (Exception e) {
+            System.out.println("testEx1, catch exception");
+            ret = false;
+            throw e;
+        } finally {
+            System.out.println("testEx1, finally; return value=" + ret);
+            return ret;
+        }
+    }
+
+    boolean testEx2() throws Exception {
+        boolean ret = true;
+        try {
+            int b = 12;
+            int c;
+            for (int i = 2; i >= -2; i--) {
+                c = b / i;
+                System.out.println("i=" + i);
+            }
+            return true;
+        } catch (Exception e) {
+            System.out.println("testEx2, catch exception");
+            ret = false;
+            throw e;
+        } finally {
+            System.out.println("testEx2, finally; return value=" + ret);
+            return ret;
+        }
+    }
+
+    public static void main(String[] args) {
+        TestFinally testException1 = new TestFinally();
+        try {
+            testException1.testEx();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
